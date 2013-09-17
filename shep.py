9b43ce1a847a34d20a5a39f65a21e9e35ba49918
Scott Walsh <swalsh@tenable.com>
2013-09-17 15:46:52 -0400

    modified

--- shep.py
+++ shep.py
@@ -5,51 +5,17 @@ Tamper with HTTP headers to your heart's content. Excellent for
 testing, or just having a good time trying to break other tools.
 
 """
-
 import SimpleHTTPServer
 import SocketServer
 import atexit
 import os
 import sys
 
-
-def cleanup(dir):
-  """Remove the directory we created on startup. 
-
-  This is to be used in conjunction with atexit, which is called on a
-  system interrupt. 
-  """
-  print "Removing directory: " + dir
-  os.rmdir(dir)
-  print "Exiting...\n"
-
-
-def tmp_dir(dir):
-  """Make a new empty directory for starting the server.
-
-  The default action of SimpleHTTPServer is start in the working
-  directory, which may contain other files or directories that we do 
-  not want to be exposed for testing purposes. 
-  """
-  if os.path.exists(dir):
-    print "The directory exists, aborting"
-    sys.exit()
-  else:
-    print "Making: " + dir
-    os.makedirs(dir)
-    print "Changing to: " + dir
-    os.chdir(dir)
-
 #We will fall back to 8000
 try:
   port = int(sys.argv[1])
 except:
   port = 8000
-
-dir = "/tmp/serve-" + str(port)
-
-tmp_dir(dir)
-atexit.register(cleanup, dir)
 
 Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 Handler.sys_version = ""
